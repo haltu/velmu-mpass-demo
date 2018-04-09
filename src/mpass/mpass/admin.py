846c081501257970eb50b16b3984a8e45d24983f
Jussi Heikkilä <jussi.heikkila@haltu.fi>
2018-04-09 15:48:30 +0300

    Add ServiceAdmin.

--- src/mpass/mpass/admin.py
+++ src/mpass/mpass/admin.py
@@ -23,8 +23,16 @@ class AuthenticationTagAdmin(TranslatableAdmin):
   ordering = ('tag_id',)
 
 
+class ServiceAdmin(TranslatableAdmin):
+  search_fields = ('service_id', 'translations__title', 'translations__description')
+  list_display = ('title', 'service_id', 'service_url')
+  date_hierarchy = 'created_at'
+  ordering = ('service_id', )
+
+
 admin.site.register(models.AuthenticationSource, AuthenticationSourceAdmin)
 admin.site.register(models.AuthenticationTag, AuthenticationTagAdmin)
+admin.site.register(models.Service, ServiceAdmin)
 
 
 # vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2
